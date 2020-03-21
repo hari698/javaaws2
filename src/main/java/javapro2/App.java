f92836c9e2bbf7488d82085ce9ef84b0475f26ee
root <root@ip-172-31-26-210.us-east-2.compute.internal>
2020-03-21 06:32:42 +0000

    new files

---
+++ src/main/java/javapro2/App.java
@@ -0,0 +1,14 @@
+package javapro2;
+
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+            int a=8,b=9,c;
+	    System.out.println( "enter the two values:");
+	    c=a+b;
+	    System.out.print(c);
+
+    }
+}
